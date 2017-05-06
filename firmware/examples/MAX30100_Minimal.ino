4f40229034b8b6b0c70ee4a56a914bcbda6ce830
viggi1000 <viggi1000@gmail.com>
2017-05-06 18:40:43 +0530

    Update MAX30100_Minimal.ino

--- firmware/examples/MAX30100_Minimal.ino
+++ firmware/examples/MAX30100_Minimal.ino
@@ -19,9 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#include <Wire.h>
 #include "MAX30100/MAX30100_PulseOximeter.h"
 #include "application.h"
-#define REPORTING_PERIOD_MS     1000
-#define slaveAddress 0x5A
 
+#define REPORTING_PERIOD_MS     1000
 
 // PulseOximeter is the higher level interface to the sensor
 // it offers:
@@ -31,8 +30,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 PulseOximeter pox;
 int BPM,sp;
 float t;
+
 uint32_t tsLastReport = 0;
-uint32_t tsLastReport1 = 0;
 
 // Callback (registered below) fired when a pulse is detected
 void onBeatDetected()
@@ -42,29 +41,42 @@ void onBeatDetected()
 
 void setup()
 {
-
     Serial.begin(115200);
-    // Initialize the PulseOximeter instance and register a beat-detected callback
+
+    Serial.println("Initializing MAX30100");
+
+    // Initialize the PulseOximeter instance
     pox.begin();
+
+    // The default current for the IR LED is 50mA and it could be changed
+    //   by uncommenting the following line. Check MAX30100_Registers.h for all the
+    //   available options.
+    // pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+
+    // Register a callback for the beat detection
     pox.setOnBeatDetectedCallback(onBeatDetected);
 }
 
 void loop()
 {
-pox.update();
-BPM=pox.getHeartRate();
-sp=pox.getSpO2();
-Serial.print("Heart rate:");
-Serial.print(BPM);
-Serial.print("bpm / SpO2:");
-Serial.print(sp);
-Serial.println("% ");
-    
-if (millis() - tsLastReport1 > 20000) {
-Particle.publish("BPM", String(BPM));
-Particle.publish("spo2", String(sp));
-Particle.publish("temp", String(t));
-tsLastReport1 = millis();
-}
-
+    // Make sure to call update as fast as possible
+    pox.update();
+	BPM=pox.getHeartRate();
+	sp=pox.getSpO2();
+	t=pox.getTemperature();
+    // Asynchronously dump heart rate and oxidation levels to the serial
+    // For both, a value of 0 means "invalid"
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.print("Heart rate:");
+        Serial.print(BPM);
+        Serial.print("bpm / SpO2:");
+        Serial.print(sp);
+        Serial.print("% / temp:");
+        Serial.print(t);
+        Serial.println("C");
+		Particle.publish("BPM", String(BPM));
+		Particle.publish("spo2", String(sp));
+		Particle.publish("temp", String(t));
+        tsLastReport = millis();
+    }
 }
