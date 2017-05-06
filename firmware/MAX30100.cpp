ceaab2b5bb55e64205232593719cd706c7682878
viggi1000 <viggi1000@gmail.com>
2017-05-06 18:33:33 +0530

    Update MAX30100.cpp

--- firmware/MAX30100.cpp
+++ firmware/MAX30100.cpp
@@ -1,6 +1,6 @@
 /*
-Port of the Oxullo Interscans library for Particle Photon/Electron.
-Work by Vignesh Ravichandran (hello@rvignesh.xyz)
+Arduino-MAX30100 oximetry / heart rate integrated sensor library
+Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,9 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 //#include <Wire.h>
-
-#include "MAX30100.h"
 #include "application.h"
+#include "MAX30100.h"
 
 MAX30100::MAX30100()
 {
@@ -27,8 +26,8 @@ MAX30100::MAX30100()
 
 void MAX30100::begin()
 {
-    Wire.begin();
-    Wire.setSpeed(CLOCK_SPEED_400KHZ);
+   Wire.begin();
+ //   Wire.setClock(I2C_BUS_SPEED);
 
     setMode(DEFAULT_MODE);
     setLedsPulseWidth(DEFAULT_PULSE_WIDTH);
@@ -46,6 +45,97 @@ void MAX30100::setLedsPulseWidth(LEDPulseWidth ledPulseWidth)
 {
     uint8_t previous = readRegister(MAX30100_REG_SPO2_CONFIGURATION);
     writeRegister(MAX30100_REG_SPO2_CONFIGURATION, (previous & 0xfc) | ledPulseWidth);
+}
+
+void MAX30100::setSamplingRate(SamplingRate samplingRate)
+{
+    uint8_t previous = readRegister(MAX30100_REG_SPO2_CONFIGURATION);
+    writeRegister(MAX30100_REG_SPO2_CONFIGURATION, (previous & 0xe3) | (samplingRate << 2));
+}
+
+void MAX30100::setLedsCurrent(LEDCurrent irLedCurrent, LEDCurrent redLedCurrent)
+{
+    writeRegister(MAX30100_REG_LED_CONFIGURATION, redLedCurrent << 4 | irLedCurrent);
+}
+
+void MAX30100::setHighresModeEnabled(bool enabled)
+{
+    uint8_t previous = readRegister(MAX30100_REG_SPO2_CONFIGURATION);
+    if (enabled) {
+        writeRegister(MAX30100_REG_SPO2_CONFIGURATION, previous | MAX30100_SPC_SPO2_HI_RES_EN);
+    } else {
+        writeRegister(MAX30100_REG_SPO2_CONFIGURATION, previous & ~MAX30100_SPC_SPO2_HI_RES_EN);
+    }
+}
+
+void MAX30100::update()
+{
+    readFifoData();
+}
+
+uint8_t MAX30100::readRegister(uint8_t address)
+{
+    Wire.beginTransmission(MAX30100_I2C_ADDRESS);
+    Wire.write(address);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MAX30100_I2C_ADDRESS, 1);
+
+    return Wire.read();
+}
+
+void MAX30100::writeRegister(uint8_t address, uint8_t data)
+{
+    Wire.beginTransmission(MAX30100_I2C_ADDRESS);
+    Wire.write(address);
+    Wire.write(data);
+    Wire.endTransmission();
+}
+
+void MAX30100::burstRead(uint8_t baseAddress, uint8_t *buffer, uint8_t length)
+{
+    Wire.beginTransmission(MAX30100_I2C_ADDRESS);
+    Wire.write(baseAddress);
+    Wire.endTransmission(false);
+    Wire.requestFrom((uint8_t)MAX30100_I2C_ADDRESS, length);
+
+    uint8_t idx = 0;
+    while (Wire.available()) {
+        buffer[idx++] = Wire.read();
+    }
+}
+
+void MAX30100::readFifoData()
+{
+    uint8_t buffer[4];
+
+    burstRead(MAX30100_REG_FIFO_DATA, buffer, 4);
+
+    // Warning: the values are always left-aligned
+    rawIRValue = (buffer[0] << 8) | buffer[1];
+    rawRedValue = (buffer[2] << 8) | buffer[3];
+}
+
+void MAX30100::startTemperatureSampling()
+{
+    uint8_t modeConfig = readRegister(MAX30100_REG_MODE_CONFIGURATION);
+    modeConfig |= MAX30100_MC_TEMP_EN;
+
+    writeRegister(MAX30100_REG_MODE_CONFIGURATION, modeConfig);
+}
+
+bool MAX30100::isTemperatureReady()
+{
+    return !(readRegister(MAX30100_REG_MODE_CONFIGURATION) & MAX30100_MC_TEMP_EN);
+}
+
+float MAX30100::retrieveTemperature()
+{
+    int8_t tempInteger = readRegister(MAX30100_REG_TEMPERATURE_DATA_INT);
+    float tempFrac = readRegister(MAX30100_REG_TEMPERATURE_DATA_FRAC);
+
+    return tempFrac * 0.0625 + tempInteger;
+}
+);
 }
 
 void MAX30100::setSamplingRate(SamplingRate samplingRate)
